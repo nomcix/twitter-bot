d7982e992bc5630814aec2a3fe3d39d487c2d251
nomcix <chasedevtools@gmail.com>
2023-01-14 17:02:17 -0800

    remove debug print

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ def main():
     try:
         logger.info("Starting app...")
         term, definition = db_connector.fetch_random_term()
-        print(term, definition)
         image_path = image_generator.create_tweet_image(term, definition) 
         tweet_poster.post_image_to_twitter(f"tweets/{image_path}.png", f"{term}")
         db_connector.update_posted(term)
